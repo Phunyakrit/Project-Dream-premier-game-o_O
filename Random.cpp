9fec3645984d820cc567516635ac8ba0b1447cd6
Tzz 5053e <tzz977035@gmail.com>
2026-02-15 00:06:13 +0700

    g

--- Random.cpp
+++ Random.cpp
@@ -1,7 +1,43 @@
 #include<iostream>
+#include<random>
+
 using namespace std;
 
-int main(){
+string RanBall(){
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<> dist(1,3);
+    
+    int x=dist(gen)+1;
+      
+    string Ball[]={"Copper","Silver","Gold","BLACK"};
 
-    cout << "my mung tae";
+    return Ball[x];
+}
+
+
+
+vector<int> RanPlayer(int a,int min_val, int max_val){
+
+  vector<int> result;
+
+    static random_device rd;
+    static mt19937 gen(rd());
+     uniform_int_distribution<> dist(min_val,max_val);
+    
+     for(int i=0; i<a; i++){
+        result.push_back(dist(gen));
+     }
+    
+    return result;
+     
+}
+
+int main(){
+     
+    string ball=RanBall();
+            
+   cout << "Your Ball is " <<ball;
+
+    
 }
